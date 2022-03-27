5f710940d90e9dd4d7129fbbbeedabc9fbe4c013
瑀行 <dingguangren@126.com>
2022-03-27 15:37:19 +0800

    feat: 增加tour(工具集)模块，word格式转换工具集

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/yuxing/goprojects
 go 1.16
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/viper v1.3.2 // indirect
 	github.com/valyala/fasthttp v1.34.0
 	go.uber.org/zap v1.21.0
 )
---
+++ tour/internal/word/word.go
@@ -0,0 +1,54 @@
+// Package word 实现单词类型转换功能：
+// - 单词全部转换为小写
+// - 单词全部转换为大写
+// - 下划线单词转换为大写驼峰单词
+// - 下划线单词转换为小写驼峰单词
+// - 驼峰单词转为下划线单词
+package word
+
+import (
+	"strings"
+	"unicode"
+)
+
+// ToUpper 单词全部转换为大写
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// ToLower 单词全部转换为小写
+func ToLower(s string) string  {
+	return strings.ToLower(s)
+}
+
+// UnderscoreToUpperCameCase 下划线单词转换为大写驼峰单词
+func UnderscoreToUpperCameCase(s string) string {
+	s = strings.Replace(s, "_", " ", -1)
+	s = strings.Title(s)
+	return strings.Replace(s, " ", "", -1)
+}
+
+// UnderscoreToLowerCamelCase 下划线单词转换为小写驼峰单词
+func UnderscoreToLowerCamelCase(s string) string {
+	s = UnderscoreToUpperCameCase(s)
+	return string(unicode.ToLower(rune(s[0]))) + s[1:]
+}
+
+// CamelCaseToUnderscore 驼峰单词转为下划线单词
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+			continue
+		}
+		if unicode.IsUpper(r) {
+			output = append(output, '_')
+		}
+		output = append(output, unicode.ToLower(r))
+	}
+	return string(output)
+}
+
+
+
