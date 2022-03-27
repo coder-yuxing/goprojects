23c2b0a490afea0b0cd2a2e828ac605004af3e4a
瑀行 <dingguangren@126.com>
2022-03-27 21:13:19 +0800

    feat: tour工具集：sql转换工具

---
+++ tour/cmd/sql.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yuxing/goprojects/tour/internal/sql2struct"
+	"log"
+)
+
+var (
+	username  string
+	password  string
+	host      string
+	charset   string
+	dbType    string
+	dbName    string
+	tableName string
+)
+
+func init()  {
+	sqlCmd.AddCommand(sql2structCmd)
+
+	sql2structCmd.Flags().StringVarP(&username, "username", "", "", "请输入数据库的账号")
+	sql2structCmd.Flags().StringVarP(&password, "password", "", "", "请输入数据库的密码")
+	sql2structCmd.Flags().StringVarP(&host, "host", "", "", "请输入数据库的HOST")
+	sql2structCmd.Flags().StringVarP(&charset, "charset", "", "utf8mb4", "请输入数据库的编码")
+	sql2structCmd.Flags().StringVarP(&dbType, "type", "", "mysql", "请输入数据库实例类型")
+	sql2structCmd.Flags().StringVarP(&dbName, "db", "", "", "请输入数据库名称")
+	sql2structCmd.Flags().StringVarP(&tableName, "table", "", "", "请输入表名称")
+}
+
+var sqlCmd = &cobra.Command{
+	Use: "sql",
+	Short: "sql转变和处理",
+	Long: "sql转换和处理",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+var sql2structCmd = &cobra.Command{
+	Use: "stuct",
+	Short: "sql转换",
+	Long: "sql转换",
+	Run: func(cmd *cobra.Command, args []string) {
+		dbInfo := &sql2struct.DBInfo{
+			DBType:   dbType,
+			Host:     host,
+			Username: username,
+			Password: password,
+			Charset:  charset,
+		}
+		dbModel := sql2struct.NewDBModel(dbInfo)
+		err := dbModel.Connect()
+		if err != nil {
+			log.Fatalf("dbModel.Connect err: %v", err)
+		}
+		columns, err := dbModel.GetColumns(dbName, tableName)
+		if err != nil {
+			log.Fatalf("dbModel.GetColumns err: %v", err)
+		}
+		template := sql2struct.NewStructTemplate()
+		templateColumns := template.AssembleColumns(columns)
+		err = template.Generate(tableName, templateColumns)
+		if err != nil {
+			log.Fatalf("tamplate.Generate err: %v", err)
+		}
+	},
+}
---
+++ tour/internal/sql2struct/mysql.go
@@ -0,0 +1,94 @@
+package sql2struct
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+type DBModel struct {
+	DBEngine *sql.DB
+	DBInfo   *DBInfo
+}
+
+// Connect 建立数据库连接
+func (m *DBModel) Connect() error {
+	var err error
+	s := "%s:%s@tcp(%s)/information_schema?" +
+		"charset=%s&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		s,
+		m.DBInfo.Username,
+		m.DBInfo.Password,
+		m.DBInfo.Host,
+		m.DBInfo.Charset,
+	)
+	m.DBEngine, err = sql.Open(m.DBInfo.DBType, dsn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetColumns 获取表中列信息
+func (m *DBModel) GetColumns(dbName, tableName string) ([]*TableColumn, error) {
+	query := "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, " +
+		"IS_NULLABLE, COLUMN_TYPE, COLUMN_COMMENT " +
+		"FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?"
+	rows, err := m.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, errors.New("没有数据")
+	}
+	defer rows.Close()
+
+	var columns []*TableColumn
+	for rows.Next() {
+		var column TableColumn
+		err := rows.Scan(&column.ColumnName, &column.DataType, &column.ColumnKey, &column.IsNullable, &column.ColumnType, &column.ColumnComment)
+		if err != nil {
+			return nil, err
+		}
+		columns = append(columns, &column)
+	}
+	return columns, nil
+}
+
+type DBInfo struct {
+	DBType   string
+	Host     string
+	Username string
+	Password string
+	Charset  string
+}
+
+type TableColumn struct {
+	ColumnName    string
+	DataType      string
+	IsNullable    string
+	ColumnKey     string
+	ColumnType    string
+	ColumnComment string
+}
+
+// DBTypeToStructType 表字段类型映射
+var DBTypeToStructType = map[string]string{
+	"int":       "int32",
+	"tinyint":   "int8",
+	"smallint":  "int",
+	"mediumint": "int64",
+	"bigint":    "int64",
+	"bit":       "int",
+	"bool":      "bool",
+	"enum":      "string",
+	"set":       "string",
+	"varchar":   "string",
+	"char":      "string",
+}
+
+func NewDBModel(info *DBInfo) *DBModel {
+	return &DBModel{DBInfo: info}
+}
