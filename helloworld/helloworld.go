591d9cc7005d21ec42c994c82cc97bdb9f989012
瑀行 <dingguangren@126.com>
2022-03-29 12:04:20 +0800

    feat: go语言函数参数值传递测试

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -4,4 +4,30 @@ import "fmt"
 
 func main() {
    fmt.Println("hello, world")
+
+   test := Test{Name: "test", Numbers: []int{1, 2, 3}}
+   fmt.Println(test.Name)
+   fmt.Println(test.Numbers[2])
+   rename(test)
+   fmt.Println(test.Name)
+   fmt.Println(test.Numbers[2])
+   rename1(&test)
+   fmt.Println(test.Name)
+   fmt.Println(test.Numbers[2])
+
+}
+
+type Test struct {
+   Name string
+   Numbers []int
+}
+
+func rename(t Test) {
+   t.Name = "rTest"
+   t.Numbers[2] = 4
+}
+
+func rename1(t *Test) {
+   t.Name = "rTest"
+   t.Numbers[2] = 5
 }
