95a5a699be51f64fe003df5e6ad6662f845c2373
瑀行 <dingguangren@126.com>
2022-03-30 14:04:45 +0800

    feat: go 方法 receiver T 与*T类型的区别测试

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -15,6 +15,19 @@ func main() {
    fmt.Println(test.Name)
    fmt.Println(test.Numbers[2])
 
+   // Go方法的实质是以方法的 receiver 参数作为第一个参数的普通函数
+   // Go方法/函数传参采用值传递
+   // 故当方法的receiver参数为 T时，实际传入的是 T类型实例的副本，因此方法内对该参数的任何修改都对原实例没有影响
+   // 当方法receiver参数为 *T时，实际传入的是T类型示实例的地址，因此方法内对该参数的操作会反映到原T类型实例上
+   var t T
+   println(t.a) // 0
+
+   t.M1()
+   println(t.a) // 0
+
+   p := &t
+   p.M2()
+   println(t.a) // 11
 }
 
 type Test struct {
